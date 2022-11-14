eeffd7d0090aa85419a774042ebf0e8edc4defea
Camarederic <camarederic@mail.ru>
2022-11-14 23:30:05 +0300

    Create a PopupMenu

--- app/src/main/java/com/example/mvvm_50/UsersAdapter.kt
+++ app/src/main/java/com/example/mvvm_50/UsersAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.mvvm_50
 
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.mvvm_50.databinding.ItemUserBinding
@@ -70,10 +72,49 @@ class UsersAdapter(
         val user = v.tag as User
         when (v.id) {
             R.id.imageViewMore -> {
-                // todo
-            }else -> {
+                showPopupMenu(v)
+            }
+            else -> {
                 actionListener.onUserDetails(user)
             }
         }
+    }
+
+    private fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(view.context, view)
+        val context = view.context
+        val user = view.tag as User
+        val position = users.indexOfFirst { it.id == user.id }
+
+        popupMenu.menu.add(0, ID_MOVE_UP, Menu.NONE, context.getString(R.string.move_up)).apply {
+            isEnabled = position > 0
+        }
+        popupMenu.menu.add(0, ID_MOVE_DOWN, Menu.NONE, context.getString(R.string.move_down)).apply {
+            isEnabled = position < users.size - 1
+        }
+        popupMenu.menu.add(0, ID_REMOVE, Menu.NONE, context.getString(R.string.remove))
+
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                ID_MOVE_UP -> {
+                    actionListener.onUserMove(user,-1)
+                }
+                ID_MOVE_DOWN -> {
+                    actionListener.onUserMove(user, 1)
+                }
+                ID_REMOVE -> {
+                    actionListener.onUserDelete(user)
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
+
+        popupMenu.show()
+    }
+
+    companion object {
+        private const val ID_MOVE_UP = 1
+        private const val ID_MOVE_DOWN = 2
+        private const val ID_REMOVE = 3
     }
 }
